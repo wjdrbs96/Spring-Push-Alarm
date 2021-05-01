9f3ee3062e0b2c7a5c0e01c91e6fdefea46bc42e
wjdrbs96 <wjdrbs966@naver.com>
2021-05-01 20:59:05 +0900

    initial commit

---
+++ src/main/java/com/example/springfcm/SpringFcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFcmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springfcm/SpringFcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
